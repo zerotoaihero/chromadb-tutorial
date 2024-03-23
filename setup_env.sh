72fca75c89f37d7eb233879134366db8041607a4
zerotoaihero <129429348+zerotoaihero@users.noreply.github.com>
2024-03-23 12:53:02 +0530

    first commit

---
+++ setup_env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+python3 -m venv zerotoaihero  
+source zerotoaihero/bin/activate
+pip3 install chromadb
+pip3 install pandas
+pip3 install sentence-transformers
+pip3 install ipywidgets
